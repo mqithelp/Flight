8e9f78fdfe352f61fa056bad454f5b11e530e779
mqjava <mqithelp@gmail.com>
2026-04-13 02:07:15 +0300

    пробуем фабрику ) накидал в майн код)

--- src/com/gridnine/testing/Main.java
+++ src/com/gridnine/testing/Main.java
@@ -1,7 +1,23 @@
 package com.gridnine.testing;
 
+import com.gridnine.testing.builder.FlightBuilder;
+import com.gridnine.testing.filter.DepartureInPastFilter;
+import com.gridnine.testing.filter.FlightFilter;
+import com.gridnine.testing.model.Flight;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        // получить список перелётов
+        List<Flight> flights = FlightBuilder.createFlights();
 
+// создать фильтр
+        FlightFilter filter = new DepartureInPastFilter();
+
+// применить фильтр
+        List<Flight> result = filter.filter(flights);
+
+// вывести в консоль
+        System.out.println(result);
     }
 }
