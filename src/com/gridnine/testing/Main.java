8c75d9e2a422e4ab3727025b5d46098baa820922
mqjava <mqithelp@gmail.com>
2026-04-19 15:01:17 +0300

    все готово. Будем приступать к тестам

--- src/com/gridnine/testing/Main.java
+++ src/com/gridnine/testing/Main.java
@@ -7,50 +7,38 @@ import com.gridnine.testing.filter.FlightFilter;
 import com.gridnine.testing.filter.GroundTimeFilter;
 import com.gridnine.testing.model.Flight;
 import com.gridnine.testing.service.FlightFilterService;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.logging.Filter;
-
-/*
-Вывод 1 — после применения правила "вылет в прошлом"
-Вывод 2 — после применения правила "прилёт раньше вылета"
-Вывод 3 — после применения правила ">2ч на земле"
- */
 
 public class Main {
     public static void main(String[] args) {
         // получить список перелётов
         List<Flight> flights = FlightBuilder.createFlights();
-        System.out.println("ℹ️ Вылеты без фильтрации ℹ️ ");
-        for (Flight flight : flights) {
-            System.out.println("🛫 " + flight);
-        }
-        System.out.println("=".repeat(120));
-// создать фильтр
+        printFilterResult("ℹ️ Вылеты без фильтрации ℹ️ ",flights);
+
+        // фильтр
         FlightFilterService service = new FlightFilterService();
+
         List<Flight> result = service.filter(flights, List.of(new ArrivalBeforeDepartureFilter()));
         printFilterResult("🧑‍✈️Результат применения фильтра \"прилёт раньше вылета\" ArrivalBeforeDepartureFilter🧑‍✈️", result);
-//        System.out.println(result);
-        System.out.println("🧑‍✈️Результат применения фильтра \"вылет в прошлом\" DepartureInPastFilter🧑‍✈️");
-        result = service.filter(flights, List.of(new DepartureInPastFilter()));
-        System.out.println(result);
-        System.out.println("🧑‍✈️Результат применения фильтра \">2ч на земле\" GroundTimeFilter🧑‍✈️");
-        result = service.filter(flights, List.of(new GroundTimeFilter()));
-        System.out.println(result);
 
-        System.out.println("🧑‍✈️Результат применения цепочки фильтров🧑‍✈️");
+        result = service.filter(flights, List.of(new DepartureInPastFilter()));
+        printFilterResult("🧑‍✈️Результат применения фильтра \"вылет в прошлом\" DepartureInPastFilter🧑‍✈️", result);
+
+        result = service.filter(flights, List.of(new GroundTimeFilter()));
+        printFilterResult("🧑‍✈️Результат применения фильтра \">2ч на земле\" GroundTimeFilter🧑‍✈️", result);
+
         List<FlightFilter> filters = new ArrayList<>();
         filters.add(new GroundTimeFilter());
         filters.add(new ArrivalBeforeDepartureFilter());
         List<Flight> results = service.filter(flights, filters);
-// вывести в консоль
-        for (Flight flight : results) {
-            System.out.println(flight);
-        }
+        printFilterResult("🧑‍✈️Результат применения цепочки фильтров🧑‍✈️", results);
+
     }
-    private static void printFilterResult(String title, List<Flight> result){
+
+    private static void printFilterResult(String title, List<Flight> result) {
         System.out.println(title);
+        result.forEach(r -> System.out.println("\t Перелет " + r));
+        System.out.println("-".repeat(80));
     }
 }
